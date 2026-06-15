de295f97b4e18ec0fef70e7d86761ccc4bf0d613
Rafaell Santana <rafaell.santana@fpf.br>
2026-06-14 23:28:39 -0400

    add login in main page

--- src/app/features/landing-page/components/navbar/navbar.component.ts
+++ src/app/features/landing-page/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
 
 @Component({
@@ -8,7 +9,13 @@ import { ButtonComponent } from '../../../../shared/components/button/button.com
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
+  private router = inject(Router);
+
   scrollTo(elementId: string) {
     document.getElementById(elementId)?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
   }
 }
